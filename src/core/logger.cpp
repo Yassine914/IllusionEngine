b8200fd9a7760754923b8a7ad902998d77817ac3
Yassine914 <yassine.shehab206@gmail.com>
2024-07-02 15:30:21 +0300

    added coloring to logger messages

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -1,23 +1,42 @@
 #include "logger.h"
 
-struct TextColor
-{
-    const char *red = "\033[31m", *lightRed = "\033", *orange = "\033", *yellow = "\033", *green = "\033",
-               *blue = "\033", *lightBlue = "\033", *resetColor = "\033[0";
-};
+#define TEXT_RED "\x1b[31m"
+#define TEXT_GREEN "\x1b[32m"
+#define TEXT_YELLOW "\x1b[33m"
+#define TEXT_BLUE "\x1b[34m"
+#define TEXT_PURPLE "\x1b[35m"
+#define TEXT_CYAN "\x1b[36m"
+#define TEXT_RESET_COLOR "\x1b[0m"
 
 namespace logger {
 
-void _log(const char *color, const char *msg)
+std::string colorize(const char *color, const char *text)
 {
+    std::string coloredText = color;
+    coloredText.append(text).append(TEXT_RESET_COLOR);
+
+    return coloredText;
+}
+
+void _log(const char *color, const char *msg, bool fileName, bool lineNumber, bool timed)
+{
+    std::cout << color;
+
+    if(timed) // TODO: add timing (need to run a timer on a seperate thread...)
+        std::cout << "";
+
+    if(fileName && lineNumber)
+        std::cout << "";
+    else if(fileName)
+        std::cout << "";
 }
 
 void LogProblem(LogLevel logLevel, const char *msg)
 {
-    switch (logLevel)
+    switch(logLevel)
     {
     case LogLevel::fatal:
-        std::cout << "\033[33;3m[FATAL]: " << __FILE__ << __LINE__ << msg << std::endl;
+        std::cout << colorize(TEXT_RED, msg);
         break;
     case LogLevel::error:
         std::cout << "\033[22;2m[ERROR]: " << __FILE__ << __LINE__ << msg << std::endl;
@@ -32,12 +51,13 @@ void LogProblem(LogLevel logLevel, const char *msg)
 
 void LogInfo(const char *msg)
 {
-    std::cout << "\033[44;1m[INFO]: " << msg << std::endl;
+    std::cout << "[" << colorize(TEXT_PURPLE, "INFO");
+    std::cout << "]: " << colorize(TEXT_PURPLE, msg) << std::endl;
 }
 
 void LogDebug(LogLevel logLevel, char *msg)
 {
-    switch (logLevel)
+    switch(logLevel)
     {
     case LogLevel::debug:
         std::cout << "\033[11;1m[DEBUG]: " << __FILE__ << __LINE__ << msg << std::endl;
